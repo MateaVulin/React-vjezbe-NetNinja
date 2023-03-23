a484a47b6550fa7f47e9540bc0482642b4133102
Matea Vulin <matea.karadza1@gmail.com>
2023-03-23 17:26:22 +0100

    useSate hook

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 const Home = () => {
+  // let name = "mario";
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
   const handleClick = (e) => {
-    console.log("Hello,ninjas", e);
+    // name = "luigi";
+    setName("luigi");
+    setAge(30);
   };
   const handleClickAgain = (name, e) => {
     console.log("hello " + name, e.target);
@@ -8,6 +14,9 @@ const Home = () => {
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click me</button>
       <button onClick={(e) => handleClickAgain(" mario ", e)}>
         Click me again
